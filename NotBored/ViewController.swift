ecfbe8a40b920c5716e77ad70b4f0c63deaa8fa4
rVilchesS <romina.vilches@mercadolibre.cl>
2022-03-18 16:30:16 -0300

    Initial Commit

---
+++ NotBored/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotBored
+//
+//  Created by Romina Fernanda Vilches Serrano on 18-03-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
