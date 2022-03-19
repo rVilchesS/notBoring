df5e625d6ca44f9d8455e446629c2696840bcb71
rVilchesS <romina.vilches@mercadolibre.cl>
2022-03-19 20:07:43 -0300

    added start screen

---
+++ NotBored/ViewController/StartScreenViewController.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+class StartScreenViewController: UIViewController {
+
+    
+    // Outlets
+    @IBOutlet weak var participantsTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // Actions
+
+    @IBAction func activitiesTappend(_ sender: Any) {
+        
+    }
+    
+    @IBAction func termsAndConditionsTappend(_ sender: Any) {
+        
+    }
+    
+    
+    // Functions
+
+}
