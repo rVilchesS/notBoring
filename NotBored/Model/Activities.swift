577b6e2ec7b78bf843c658eafbf62ff40ed03970
rVilchesS <romina.vilches@mercadolibre.cl>
2022-03-25 03:18:18 -0300

    Add pods, alamofire and API consumption

---
+++ NotBored/Model/Activities.swift
@@ -0,0 +1,8 @@
+//
+//  Activities.swift
+//  NotBored
+//
+//  Created by Romina Fernanda Vilches Serrano on 24-03-22.
+//
+
+import Foundation
---
+++ NotBored/Service/ActivityService.swift
@@ -0,0 +1,8 @@
+//
+//  ActivityService.swift
+//  NotBored
+//
+//  Created by Romina Fernanda Vilches Serrano on 25-03-22.
+//
+
+import Foundation
